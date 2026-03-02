2a179f9b8cae0a4cc2d6325a0b1217f3ecee7855
AngilD <angildismas2014@gmail.com>
2026-03-02 18:20:55 +0300

    Switch to Railway DATABASE_URL

--- db.js
+++ db.js
@@ -1,19 +1,27 @@
-import pkg from 'pg';
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+
+// const pool = new Pool({
+//  host: process.env.DB_HOST,
+//  port: process.env.DB_PORT,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//  database: process.env.DB_NAME,
+//  max: 20,
+// });
+import pkg from "pg";
 const { Pool } = pkg;
-
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-
 const pool = new Pool({
- host: process.env.DB_HOST,
- port: process.env.DB_PORT,
- user: process.env.DB_USER,
- password: process.env.DB_PASSWORD,
- database: process.env.DB_NAME,
- max: 20,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+
+
 
 //  console.log("DB_PASSWORD",process.env.DB_PASSWORD)
 
